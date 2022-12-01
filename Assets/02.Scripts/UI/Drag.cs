cd850aa2452fa110f567826a5a78a4fd46c4e2b1
KUROJIWOON <114848984+DINNYARING@users.noreply.github.com>
2022-12-01 16:51:54 +0900

    Inventory Drag&Drop
    Added Components
    Written Scripts : Drop.cs, Drag.cs

--- Assets/02.Scripts/UI/Drag.cs
+++ Assets/02.Scripts/UI/Drag.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler
+public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     private Transform itemTr;
     private Transform inventoryTr;
+    [SerializeField]
+    private Transform itemListTr;
+    private CanvasGroup canvasGroup;
 
+    public static GameObject draggingItem = null;
 
     void Start()
     {
         itemTr = GetComponent<Transform>();
         inventoryTr = GameObject.Find("Inventory").GetComponent<Transform>();
+        itemListTr = GameObject.Find("ItemList").GetComponent<Transform>();
+
+        // Canvas Group 컴포넌트 추출
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
     // 드래그 이벤트
@@ -27,5 +35,24 @@ public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler
     {
         // 부모를 Inventory로 변경
         this.transform.SetParent(inventoryTr);
+        // 드래그가 시작되면 드래그되는 아이템 정보를 저장함
+        draggingItem = this.gameObject;
+        // 드래그가 시작되면 다른 UI 이벤트를 받지 않도록 설정
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    // 드래그가 종료됐을 때 한 번 호출되는 이벤트
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // 드래그가 종료되면 드래그 아이템을 null로 변경
+        draggingItem = null;
+        // 드래그가 종료되면 다른 UI 이벤트를 받도록 설정
+        canvasGroup.blocksRaycasts = true;
+
+        // 슬롯에 드래그하지 않았을 때 원래대로 ItemList로 되돌린다.
+        if (itemTr.parent == inventoryTr)
+        {
+            itemTr.SetParent(itemListTr.transform);
+        }
     }
 }
