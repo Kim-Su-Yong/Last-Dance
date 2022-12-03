17c80fb7dc9079add0350e8f1f22b4155b48bbc5
cykim <114848800+kimchanyou@users.noreply.github.com>
2022-12-03 15:47:44 +0900

    UIManager Script 수정
    쿨타임 추가

--- Assets/02.Scripts/UI/UIManager.cs
+++ Assets/02.Scripts/UI/UIManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Image coolImg;
+    //public float inittime = 0f;
+    //public float cooltime = 5f;
+    public Text cooltxt;
+    public bool isCool;
+    public static UIManager uIManager;
+
+
     void Start()
     {
-        
+        coolImg.fillAmount = 1f;
+        coolImg.enabled = false;
+        cooltxt.enabled = false;
+        isCool = true;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Z) && isCool)
+        {
+            isCool = false;
+            cooltxt.enabled = true;
+            coolImg.enabled = true;
+            StartCoroutine(CoolTime(3f));
+        }
+    }
+    IEnumerator CoolTime(float cool)
+    {
+        while (cool >= 0f)
+        {
+            cool -= Time.deltaTime;
+            coolImg.fillAmount = (cool / 3f);
+            cooltxt.text = cool.ToString("0.0");
+            yield return new WaitForFixedUpdate();
+        }
+        coolImg.fillAmount = 1f;
+        coolImg.enabled = false;
+        cooltxt.enabled = false;
+        isCool = true;
     }
 }
