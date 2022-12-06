be5c9a24ec16e627ef6634c3dd2723949fa0e34d
KUROJIWOON <114848984+DINNYARING@users.noreply.github.com>
2022-12-06 17:54:47 +0900

    Monster 스크립트 내용 추가
    MonsterAI.cs
    MoveAgent.cs
    스크립트 내용 추가

--- Assets/02.Scripts/Monster/MonsterAI.cs
+++ Assets/02.Scripts/Monster/MonsterAI.cs
@@ -4,14 +4,101 @@ using UnityEngine;
 
 public class MonsterAI : MonoBehaviour
 {
-
-    void Start()
+    public enum State
     {
-        
+        PATROL,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+    public State state = State.PATROL;
+    private Transform playerTr;
+    private Transform monsterTr;
+
+    private Animator animator;
+
+    public float attackDist = 5.0f;
+    public float traceDist = 10.0f;
+
+    public bool isDie = false;
+
+    private WaitForSeconds ws;
+    private MoveAgent moveAgent;
+
+    // 프리팹 특성 상 BlendTree > Locomotion 이외에는 Any State -> Trigger 
+    //private readonly int hashMove = Animator.StringToHash("IsMove");
+    private readonly int hashSpeed = Animator.StringToHash("Locomotion");
+    private readonly string playerTag = "Player";
+
+    void Awake()
+    {
+        var player = GameObject.FindGameObjectWithTag(playerTag);
+        if (player != null)
+        {
+            playerTr = player.GetComponent<Transform>();
+        }
+        monsterTr = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+        moveAgent = GetComponent<MoveAgent>();
+
+        ws = new WaitForSeconds(0.3f);
     }
 
+    void OnEnable()
+    {
+        StartCoroutine(CheckState());
+        StartCoroutine(Action());
+    }
+
+    IEnumerator CheckState()
+    {
+        // 몬스터 사망 전까지
+        while (!isDie)
+        {
+            if (state == State.DIE) yield break;
+
+            float dist = Vector3.Distance(playerTr.position, monsterTr.position);
+
+            if (dist <= attackDist)
+            {
+                state = State.ATTACK;
+            }
+            else if (dist <= traceDist)
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.PATROL;
+            }
+            yield return ws;
+        }
+        yield return ws;
+    }
+    IEnumerator Action()
+    {
+        while (!isDie)
+        {
+            yield return ws;
+            switch (state)
+            {
+                case State.PATROL:
+                    moveAgent.patrolling = true;
+                    break;
+                case State.TRACE:
+                    moveAgent.traceTarget = playerTr.position;
+                    break;
+                case State.ATTACK:
+                    moveAgent.Stop();
+                    break;
+                case State.DIE:
+                    moveAgent.Stop();
+                    break;
+            }
+        }
+    }
     void Update()
     {
-        
+        animator.SetFloat(hashSpeed, moveAgent.speed);
     }
 }
--- Assets/02.Scripts/Monster/MoveAgent.cs
+++ Assets/02.Scripts/Monster/MoveAgent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,51 @@ public class MoveAgent : MonoBehaviour
     // 다음 순찰지점 배열 Index
     public int nextIdx;
 
+    private readonly float patrolSpeed = 1.5f;
+    private readonly float traceSpeed = 4.0f;
+    private float damping = 1.0f;
+
     private NavMeshAgent agent;
+    private Transform monsterTr;
+
+    private bool _patrolling;
+    // patrolling 프로퍼티 정의 (getter, setter)
+    public bool patrolling
+    {
+        get { return _patrolling; }
+        set {
+            _patrolling = value;
+            if (_patrolling)
+            {
+                agent.speed = patrolSpeed;
+                damping = 1.0f; // 순찰 상태 회전계수
+                MoveWayPoint();
+            }
+        }
+    }
+    private Vector3 _traceTarget;
+    public Vector3 traceTarget
+    {
+        get { return _traceTarget; }
+        set {
+            _traceTarget = value;
+            agent.speed = traceSpeed;
+            damping = 7.0f; // 추적 상태 회전계수
+            TraceTarget(_traceTarget);
+        }
+    }
+    public float speed
+    {
+        get { return agent.velocity.magnitude; }
+    }
 
     void Start()
     {
+        monsterTr = GetComponent<Transform>();
         agent = GetComponent<NavMeshAgent>();
         agent.autoBraking = false;
+        agent.updateRotation = false;
+        agent.speed = patrolSpeed;
 
         // 하이어라키에서 WayPointGroup 오브젝트 추출
         var group = GameObject.Find("WayPointGroup");
@@ -26,10 +66,46 @@ public class MoveAgent : MonoBehaviour
             // 배열 첫 번째 항목 삭제(부모)
             wayPoints.RemoveAt(0);
         }
+
+        MoveWayPoint();
     }
 
+    void MoveWayPoint()
+    {
+        if (agent.isPathStale) return;
+
+        agent.destination = wayPoints[nextIdx].position;
+        agent.isStopped = false;
+    }
+
+    void TraceTarget(Vector3 pos)
+    {
+        if (agent.isPathStale) return;
+        agent.destination = pos;
+        agent.isStopped = false;
+    }
+    public void Stop()
+    {
+        agent.isStopped = true;
+        agent.velocity = Vector3.zero;
+        _patrolling = false;
+    }
     void Update()
     {
-        
+        if (agent.isStopped == false)
+        {
+            Quaternion rot = Quaternion.LookRotation(agent.desiredVelocity);
+            monsterTr.rotation = Quaternion.Slerp(monsterTr.rotation, rot, Time.deltaTime * damping);
+        }
+        // 순찰 모드가 아닐 경우 이후 로직을 실행하지 않음
+        if (!_patrolling) return;
+
+        // 목적지 도착 여부 계산
+        if (agent.velocity.sqrMagnitude >= 0.2f * 0.2f
+            && agent.remainingDistance <= 0.5f)
+        {
+            nextIdx = ++nextIdx % wayPoints.Count;
+            MoveWayPoint();
+        }
     }
 }
