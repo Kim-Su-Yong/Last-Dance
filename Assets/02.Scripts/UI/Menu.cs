c22f6383e2a3fd1deee0d7b098ff28bdd0acdab5
KimBomnim <108743218+KimBomnim@users.noreply.github.com>
2022-12-13 19:13:22 +0900

    메뉴 UI 변경 완료
    메뉴 UI 변경 완료

--- Assets/02.Scripts/UI/Menu.cs
+++ Assets/02.Scripts/UI/Menu.cs
@@ -6,66 +6,98 @@ public class Menu : MonoBehaviour
 {
     public static Menu instance;
 
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            DontDestroyOnLoad(this.gameObject);
-            instance = this;
-        }
-        else
-        {
-            Destroy(this.gameObject);
-        }
-    }
-    public GameObject go;
     public SoundManager theSound;
 
     public string call_sound;
     public string cancel_sound;
 
-    public OrderManager theOrder;
-
-    private bool activated;
-
-    private StandardInput theStandard;
+    public GameObject pauseImg;
+    public RectTransform pauseMenu;
+    public GameObject player;
+    public RectTransform soundMenu;
+    public RectTransform screenMenu;
 
     void Start()
     {
-        theStandard = FindObjectOfType<StandardInput>();
-    }
-    public void Exit()
-    {
-        Application.Quit();
-    }
-    public void Continue()
-    {
-        activated = false;
-        go.SetActive(false);
-        theSound.Play(cancel_sound);
-        Time.timeScale = 1;
-        theStandard.cursorLocked = true;
+        player = GameObject.FindGameObjectWithTag("Player").gameObject;
+        pauseImg = GameObject.Find("Canvas_Menu").transform.GetChild(0).gameObject;
+        pauseMenu = pauseImg.transform.GetChild(0).GetComponent<RectTransform>();
+        soundMenu = pauseImg.transform.GetChild(1).GetComponent<RectTransform>();
+        screenMenu = pauseImg.transform.GetChild(2).GetComponent<RectTransform>();
     }
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
-            activated = !activated;
-
-            if(activated)
-            {
-                go.SetActive(true);
-                theSound.Play(call_sound);
-                //Time.timeScale = 0;
-                theStandard.cursorLocked = false;
-            }
-            else
-            {
-                go.SetActive(false);
-                theSound.Play(cancel_sound);
-                //Time.timeScale = 1;
-                theStandard.cursorLocked = true;
-            }
+            theSound.Play(call_sound);
+            Pause();
         }
+    }
+    public void Pause()
+    {
+        if (pauseImg.gameObject.activeInHierarchy == false) // 캔버스 안에 RectTransform 오브젝트 활성화 / 비활성화
+        {
+            pauseImg.gameObject.SetActive(true);
+            if (pauseMenu.gameObject.activeInHierarchy == false)
+            {
+                pauseMenu.gameObject.SetActive(true);
+            }
+            Time.timeScale = 0f; // 게임 정지
+        }
+        else
+        {
+            pauseImg.gameObject.SetActive(false);
+            screenMenu.gameObject.SetActive(false);
+            soundMenu.gameObject.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+    public void Close()
+    {
+        pauseImg.gameObject.SetActive(false);
+        screenMenu.gameObject.SetActive(false);
+        soundMenu.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+
+    }
+    public void Sounds(bool isopen)
+    {
+        if (isopen == true)
+        {
+            soundMenu.gameObject.SetActive(true);
+            pauseMenu.gameObject.SetActive(false);
+        }
+        if (!isopen)
+        {
+            soundMenu.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
+        }
+    }
+    public void ScreenSetting(bool isopen)
+    {
+        if (isopen == true)
+        {
+            screenMenu.gameObject.SetActive(true);
+            soundMenu.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(false);
+        }
+        if (!isopen)
+        {
+            screenMenu.gameObject.SetActive(false);
+            soundMenu.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
+        }
+    }
+    public void GoToTitle()
+    {
+        pauseImg.gameObject.SetActive(false);
+        screenMenu.gameObject.SetActive(false);
+        soundMenu.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+        SceneLoader.Instance.LoadScene("TitleScene");
+    }
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
