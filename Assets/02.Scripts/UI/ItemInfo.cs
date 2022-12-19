587bc462752c3a042e953074e8516fcffbd8f35f
cykim <114848800+kimchanyou@users.noreply.github.com>
2022-12-19 17:54:12 +0900

    인벤토리 구현
    아이템 먹으면 장비, 소비, 기타로 분류되어서 들어가고 장비템은 장비창에 해당 타입으로 장착가능.

--- Assets/02.Scripts/UI/ItemInfo.cs
+++ Assets/02.Scripts/UI/ItemInfo.cs
@@ -2,40 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[System.Serializable]
 public class ItemInfo : MonoBehaviour
 {
+    //public static ItemInfo itemInfo;
 
+    public int itemID; //아이템의 고유 ID값. 중복 불가
+    public string itemName; //아이템의 이름. 중복 가능
+    public string itemDescription; //아이템 설명
+    public int itemCount; //소지 갯수
+    public Sprite itemIcon; //아이템의 아이콘.
     // 아이템 1차 분류
-    public enum ItemCategory { Equip, Consume, Etc };
-    public ItemCategory itemCategory;
-
-    public static ItemInfo itemInfo;
-
-    // 아이템 2차 분류
-    public enum ItemType
+    public enum ItemType { Equip, Consume, ETC };
+    // 장비 2차 분류
+    public enum EquipType
     {
-        Goggles     = 0,
-        Helmet      = 1,
-        Totem       = 2,
-        Weapon      = 3,
-        Armor       = 4,
-        Gloves      = 5,
-        Shield      = 6,
-        Boots       = 7,
-        Backpack    = 8,
-        Potion      = 9,
-        etc         = 11
-    };
+        None, Weapon, Helmet, Armor, Boots, Totem
+    }
     public ItemType itemType;
+    public EquipType equipType;
 
-
-    void Start()
+    public ItemInfo(int _itemID, string _itemName, string _itemDes, ItemType _itemType, int _itemCount = 1)
     {
-        
+        itemID = _itemID;
+        itemName = _itemName;
+        itemDescription = _itemDes;
+        itemType = _itemType;
+        itemCount = _itemCount;
+        itemIcon = Resources.Load("ItemImage/" + _itemID.ToString(), typeof(Sprite)) as Sprite;
     }
 
-    void Update()
+    public ItemInfo(int _itemID, string _itemName, string _itemDes, ItemType _itemType, EquipType _equipType, int _itemCount = 1)
     {
-        
+        itemID = _itemID;
+        itemName = _itemName;
+        itemDescription = _itemDes;
+        itemType = _itemType;
+        equipType = _equipType;
+        itemCount = _itemCount;
+        itemIcon = Resources.Load("ItemImage/" + _itemID.ToString(), typeof(Sprite)) as Sprite;
     }
+
+
+    //// 아이템 2차 분류
+    //public enum ItemType
+    //{
+    //    Goggles     = 0,
+    //    Helmet      = 1,
+    //    Totem       = 2,
+    //    Weapon      = 3,
+    //    Armor       = 4,
+    //    Gloves      = 5,
+    //    Shield      = 6,
+    //    Boots       = 7,
+    //    Backpack    = 8,
+    //    Potion      = 9,
+    //    etc         = 11
+    //};
 }
