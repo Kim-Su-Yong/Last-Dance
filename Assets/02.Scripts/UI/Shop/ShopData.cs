acbfe2ca14bcfa5ea76aaaed39f382e3ae94e0b1
KimBomnim <108743218+KimBomnim@users.noreply.github.com>
2022-12-19 18:41:15 +0900

    충돌난 부분 수정 완료
    찬유형이랑 스크립트 충돌난 부분 수정 완료

--- Assets/02.Scripts/UI/Shop/ShopData.cs
+++ Assets/02.Scripts/UI/Shop/ShopData.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class ShopData : MonoBehaviour
 {
-    public List<Item_cy> stocks = new List<Item_cy>();
+    public List<ItemInfo> stocks = new List<ItemInfo>();
     public bool[] soldOuts;
     void Start()
     {
-        //stocks.Add(DatabaseManager_cy.instance.itemList[0]);
-        //stocks.Add(DatabaseManager_cy.instance.itemList[1]);
-        //stocks.Add(DatabaseManager_cy.instance.itemList[2]);
-        //stocks.Add(DatabaseManager_cy.instance.itemList[3]);
-        //stocks.Add(DatabaseManager_cy.instance.itemList[4]);
+        stocks.Add(DatabaseManager_cy.instance.itemList[0]);
+        stocks.Add(DatabaseManager_cy.instance.itemList[1]);
+        stocks.Add(DatabaseManager_cy.instance.itemList[2]);
+        stocks.Add(DatabaseManager_cy.instance.itemList[3]);
+        stocks.Add(DatabaseManager_cy.instance.itemList[4]);
         soldOuts = new bool[stocks.Count];
         for(int i = 0; i < soldOuts.Length; i++)
         {
--- Assets/02.Scripts/UI/Shop/ShopSlot.cs
+++ Assets/02.Scripts/UI/Shop/ShopSlot.cs
@@ -6,10 +6,16 @@ using UnityEngine.EventSystems;
 public class ShopSlot : MonoBehaviour
 {
     public int slotnum;
-    public Item_cy item;
+    public ItemInfo item;
     public Image itemIcon;
     public bool soldOut = false;
     InventoryUI inventoryUI;
+
+    public GameObject[] ConsumeSlots;
+    private void Awake()
+    {
+        ConsumeSlots = GameObject.FindGameObjectsWithTag("Consume");
+    }
     public void Init(InventoryUI lui)
     {
         inventoryUI = lui;
@@ -34,7 +40,7 @@ public class ShopSlot : MonoBehaviour
             //if (DatabaseManager.instance.money >= item.itemCost && !soldOut && Inventory.instance.items.Count < Inventory.instance.SlotCnt)
             //{
                 //DatabaseManager.instance.money -= item.itemCost;
-                //Inventory_cy.instance.AddItem();
+                Inventory_cy.instance.AddItem(ConsumeSlots); //좀 더 수정해야함
                 soldOut = true;
                 inventoryUI.Buy(slotnum);
                 UpdateSlotUI();
